65b03e3b80c5eb251f75e302307ad4939b7023a7
x-mfe <125447515+x-mfe@users.noreply.github.com>
2023-03-16 11:17:59 +0200

    harjotukisa :D
    joo

--- joo.js
+++ joo.js
@@ -1,1 +1,11 @@
+/*var me = "Jonne"
+var profession = "opiskelija"
+var education = "amiksessa"
+var hometown = "vihdissä"
 
+var whoami = "Olen " + me +" olen " + profession + " " + education + " asun " + hometown 
+alert (whoami)
+
+console.warn("WARNING STRANGER DANGER")
+console.log("Name". name. profession.)
+console.error("ERROR ERROR ERROR ERROR")*/
